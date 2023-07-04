f08cd6c042c7324a0464f6a410ab945ef45a2f69
rafiulaanam <rafiulaanam@gmail.com>
2023-07-04 17:19:42 +0600

    'info-added'

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,7 @@
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Anam Developer",
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import ClientReviews from "./components/ClientReviews/ClientReviews";
 import ContactMe from "./components/ContactMe/ContactMe";
 import LatestBlogs from "./components/LatestBlogs/LatestBlogs";
 import MyAllSkills from "./components/MyAllSkills/MyAllSkills";
-import MyPortfolio from "./components/MyPortfolio/MyPortfolio";
 import MyResume from "./components/MyResume/MyResume";
 import MyServices from "./components/MyServices/MyServices";
 import MyWorks from "./components/MyWorks/MyWorks";
@@ -18,7 +17,6 @@ export default function Home() {
       <MyServices/>
       <MyResume/>
       <MyWorks/>
-      {/* <MyPortfolio/> */}
       <ClientReviews/>
       <LatestBlogs/>
       <ContactMe/>
